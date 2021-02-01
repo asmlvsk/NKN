2f9e68d8ea19f856c041fc9810b57eb2fbd7aef5
ArtemS <you@example.com>
2021-02-01 12:53:19 +0100

    Commit

---
+++ NKNProject/Models/TrackData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NKNProject.Models
+{
+    public class TrackData
+    {
+        [Key]
+        public Guid TrackId { get; set; }
+        public string TrackName { get; set; }
+        public string TrackGenre { get; set; }
+        public DateTime TrackReleaseDate { get; set; }
+    }
+}
